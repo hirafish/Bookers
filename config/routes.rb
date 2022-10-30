29f1ad0725d27dddf367ad1d4842d810bfdb9044
hirafish <hirawo@icloud.com>
2022-10-30 19:34:58 +0000

    [Add]edit delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get "books" => "books#index"
-  post "books" => "books#create"
-  get 'books/:id' => "books#show", as: "book"
-  get 'books/edit'
   get '' => 'homes#top'
+  resources :books
 end
