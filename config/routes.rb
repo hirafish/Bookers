847494a2c27768eabf01e768258af17536e8f234
hirafish <hirawo@icloud.com>
2022-10-30 18:36:58 +0000

    [Add]submit function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'books' => "books#new"
-  get 'books/index'
-  get 'books/show'
+  get "books" => "books#index"
+  post "books" => "books#create"
+  get 'books/:id' => "books#show", as: "book"
   get 'books/edit'
   get '' => 'homes#top'
 end
