514d12dd31d57ba2f66c227bb2c40c72e4509813
hirafish <hirawo@icloud.com>
2022-10-30 21:32:27 +0000

    [Add] succesful

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -23,24 +23,24 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     flash[:notice] = "Book was successfully updated."
     redirect_to "/books"
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render edit_book_path(@book.id)
+      render :edit
     end
   end
-  
+
   private
   # ストロングパラメータ
   def book_params
